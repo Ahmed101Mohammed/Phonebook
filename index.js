ee6af36492e4820b85abb21c78caeb555141bb85
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2024-01-16 14:12:38 +0200

    Add get rout for '/api/person/:id' requests E:3.3

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ app.get('/info',(req,res)=>{
   const message = `<p>Phonebook has info for ${phoneBookData.length} people.</p> <p>${new Date()}</p>`;
   res.send(message);
 })
+
 app.get('/api/persons/',(req,res)=>
 {
   res.json(phoneBookData)
+})
+
+app.get('/api/persons/:id',(req,res)=>
+{
+  const id = Number(req.params.id);
+  const person = phoneBookData.find(person=>person.id===id);
+  if(person)
+  {
+    res.json(person)
+  }
+  else
+  {
+    res.status(404).end();
+  }
 })
 const PORT = 3001;
 app.listen(PORT,()=>console.log(`Server runing at port:${PORT}`))
