31b0c5a0c2cbe97ead1a24f5c8990e76148a318a
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2024-01-16 14:08:08 +0200

    Add get roat for /info request. E:3.2

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const phoneBookData = require('./data');
 const app = express();
 app.use(express.json());
 
+app.get('/info',(req,res)=>{
+  const message = `<p>Phonebook has info for ${phoneBookData.length} people.</p> <p>${new Date()}</p>`;
+  res.send(message);
+})
 app.get('/api/persons/',(req,res)=>
 {
   res.json(phoneBookData)
 })
-const PORT = 3030;
+const PORT = 3001;
 app.listen(PORT,()=>console.log(`Server runing at port:${PORT}`))
